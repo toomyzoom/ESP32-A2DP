51ca72adb486fd7da381af697a597cce7675a29a
Chris Johnson <tnifc@protonmail.com>
2022-02-06 17:42:06 -0500

    Fix esp-idf version check

--- src/config.h
+++ src/config.h
@@ -2,20 +2,28 @@
 
 #define DEPRECATED __attribute__((deprecated))
 
-
+// Enable CURRENT_ESP_IDF if we are using a current version of ESP IDF e.g. 4.3
+// ESP Arduino 2.0 is using ESP IDF 4.4
 #if __has_include("esp_arduino_version.h")
 #include "esp_arduino_version.h"
+#  if ESP_ARDUINO_VERSION_MAJOR >= 2
+#    define CURRENT_ESP_IDF
+#  endif
+#endif
+
+#if __has_include("esp_idf_version.h")
+#include "esp_idf_version.h"
+#  if ESP_IDF_VERSION_MAJOR >= 4
+#    ifndef CURRENT_ESP_IDF
+#      define CURRENT_ESP_IDF
+#    endif
+#  endif
 #endif
 
 #ifndef AUTOCONNECT_TRY_NUM
 #define AUTOCONNECT_TRY_NUM 1000
 #endif
 
-// Enable CURRENT_ESP_IDF if we are using a current version of ESP IDF e.g. 4.3
-// ESP Arduino 2.0 is using ESP IDF 4.4
-#if ESP_IDF_VERSION_MAJOR >= 4 || ESP_ARDUINO_VERSION_MAJOR >= 2
-#define CURRENT_ESP_IDF
-#endif
 
 // Compile ESP32C3
 #if __has_include("sdkconfig.h")
